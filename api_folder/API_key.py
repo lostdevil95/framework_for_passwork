6c5e532688b82c81aa6b31d379c7923bd617c643
lostdevil95 <48276079+lostdevil95@users.noreply.github.com>
2021-12-28 16:33:28 +0300

    Update API_key.py

--- api_folder/API_key.py
+++ api_folder/API_key.py
@@ -1,6 +1,6 @@
 class ApiData:
-    api_key = 'wNxRewGJZM2xe0xZgePsPB3BwOUjpsenRWXiIlQfEAsnOqR7NtkxtEeYMc41'  # Введите ваш api_key
-    vault_ASCII_id = '61c180c971c24646ad61d482'
-    folderID_admins = '61c1b62476768d09ad7436fe'
-    folderID_users = '61c3262224550d6f9a4961de'
-    folderID_security = '61c3271824550d6f9a4961e8'
+    api_key = '*********'  # Введите ваш api_key
+    vault_ASCII_id = '*********' # id сейфа
+    folderID_admins = '*****' # id папки
+    folderID_users = '******' # id папки
+    folderID_security = '*******' # id папки
